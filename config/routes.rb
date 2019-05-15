5865132e7a97504061175d6d182d6177914de3f6
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-05-15 08:16:12 +0300

    Add routes to cats controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :people, only: [:index, :show]
+    resources :cats, only: [:index, :show]
   end
 
 end
