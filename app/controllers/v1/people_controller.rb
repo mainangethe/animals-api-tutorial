cc4deeabcc581c1a5336cb0de204158108ae87fb
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-05-15 07:57:12 +0300

    Fix index model error

--- app/controllers/v1/people_controller.rb
+++ app/controllers/v1/people_controller.rb
@@ -1,10 +1,10 @@
 class V1::PeopleController < ApplicationController
   def index
-    render json: People.all
+    render json: Person.all
   end
 
   def show
     render json: Person.find(params[:id])
   end
-  
+
 end
